a1e4602e57da2b2f8d960e6768d18a67b4f7d7d2
ShushantSarve <78533665+ShushantSarve@users.noreply.github.com>
2022-11-04 18:11:57 +0530

    Time: 27 ms (48.55%), Space: 8.3 MB (77.22%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -9,14 +9,16 @@ public:
             mp[s[i]]++;
         }
         
-        // put it in vector to
+        // put it in vector to sort the map
         vector<pair<int, char>> v;
         for(auto i : mp){
             v.push_back({i.second, i.first});
         }
 
+        //sorting
         sort(v.begin(), v.end(), greater<>());
 
+        //pushing sorted characters to empty ans string
         string ans = "";
         for(auto i : v){
             int count = i.first;
