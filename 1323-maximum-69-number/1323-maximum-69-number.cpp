47881387beebfd40978aad0a95e5ab1bd2df7af9
ShushantSarve <78533665+ShushantSarve@users.noreply.github.com>
2022-11-07 08:36:44 +0530

    Time: 2 ms (45.34%), Space: 6 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -4,12 +4,15 @@ public:
         
         vector<int> v;
         int temp = num;
+        
+        //breaking the number
         while(temp){
             int a = temp%10;
             temp /= 10;
             v.push_back(a);
         }
         
+        //reordering the number in vector
         int i=0, j=v.size()-1;
         while(i<j){
             swap(v[i], v[j]);
@@ -17,6 +20,7 @@ public:
             j--;
         }
         
+        //checking for least significant '6'
         for(int i=0; i<v.size(); i++){
             if(v[i] == 6){
                 v[i] = 9;
@@ -24,10 +28,12 @@ public:
             }
         }
         
+        //constructing the new number
         int ans = 0;
         for(int i=0; i<v.size(); i++){
             ans = (ans*10) + v[i];
         }
+        
         return ans;
     }
 };
