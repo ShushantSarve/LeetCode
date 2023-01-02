2c8b26b8f05256e1f2e2a87d026d91032a6a58a8
ShushantSarve <78533665+ShushantSarve@users.noreply.github.com>
2023-01-02 18:53:50 +0530

    Added solution - LeetHub

--- Largest BST - GFG/largest-bst.cpp
+++ Largest BST - GFG/largest-bst.cpp
@@ -102,46 +102,46 @@ struct Node {
 class Solution{
     public:
     class info{
-    public:
-        int maxi;
-        int mini;
-        bool isBST;
-        int size;
-};
-
-info solve(Node * root, int &ans){
+        public:
+            int maxi;
+            int mini;
+            bool isBST;
+            int size;
+    };
     
-    if(root == NULL){
-        return {INT_MIN, INT_MAX, 1, 0};
+    info solve(Node* root, int &ans){
+        
+        //  base case
+        if(root == NULL){
+            return {INT_MIN, INT_MAX, true, 0};
+        }
+        
+        info left = solve(root->left, ans);
+        info right = solve(root->right, ans);
+        
+        info currNode;
+        currNode.size = left.size + right.size + 1;
+        currNode.maxi = max(root->data, right.maxi);
+        currNode.mini = min(root->data, left.mini);
+        
+        if(left.isBST && right.isBST && root->data > left.maxi && root->data < right.mini){
+            currNode.isBST = true;
+        }
+        else{
+            currNode.isBST = false;
+        }
+        
+        if(currNode.isBST){
+            ans = max(ans, currNode.size);
+        }
+        return currNode;
+        
     }
-    
-    info left = solve(root->left, ans);
-    info right = solve(root->right, ans);
-    
-    info currNode;
-    currNode.size = left.size + right.size + 1;
-    currNode.maxi = max(root->data, right.maxi);
-    currNode.mini = min(root->data, left.mini);
-    
-    if(left.isBST && right.isBST && root->data < right.mini && root->data > left.maxi){
-        currNode.isBST = true;
-    }
-    else{
-        currNode.isBST = false;
-    }
-    
-    // update ans
-    if(currNode.isBST){
-        ans = max(ans, currNode.size);
-    }
-    return currNode;
-}
-    
     int largestBst(Node *root)
     {
     	int maxSize = 0;
-        info temp = solve(root, maxSize);
-        return maxSize;
+    	solve(root, maxSize);
+    	return maxSize;
     }
 };
 
